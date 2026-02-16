804eb4b5dbb609ba1f34b0697c56231246087b2e
PatelKamya <kamyapatel303@gmail.com>
2026-02-16 22:40:48 +0530

    react icons installed

--- client/src/components/CourseCard.jsx
+++ client/src/components/CourseCard.jsx
@@ -3,24 +3,17 @@ import React from 'react'
 const CourseCard = () => {
   return (
     <div>
-      <div className=' ms-4 gap-2 grid grid-cols-4 justify-evenly'>
-        <div className='bg-blue-300 rounded-lg h-60'>
+      <div className=' m-7 gap-2 grid grid-cols-4 justify-evenly'>
+        <div className='bg-blue-300 rounded-lg h-60 hover:shadow-lg hover:border-1'>
           <div>
             <h5 className='m-4'>course card</h5>
           </div>
-          <div id='on-hover' className='relative mt-40 
-          overflow-hidden translate-all duration-300 
-          hover:shadow-[0_15px_75px_-10px_rgba(0,0,0,0.4)] group'>
-            <div className=''>
-              <p>python programming</p>
-              <div>
-
-              </div>
-            </div>
+          <div className=''>
+            
           </div>
         </div>
       </div>
-      <span>Course Name</span>
+      
     </div>
   )
 }
