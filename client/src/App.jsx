67759495325dc75b62064734181fc55039a0e1f7
PatelKamya <kamyapatel303@gmail.com>
2026-02-19 23:11:46 +0530

    navbar and card combine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,23 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import CourseCard from './components/CourseCard'
+import Layout from './components/Layout';
 
-  function App() {
+function App() {
 
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Navbar />} />
-          <Route path='/card' element={<CourseCard />} />
+          <Route path='/' element={<Layout />} >
+            <Route index element={<CourseCard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
 
-      </>
-    )
-  }
+    </>
+  )
+}
 
 export default App
